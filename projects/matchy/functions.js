c01c7649070ceb9c9d8c6dda2e9fd1003db8e1a5
Cristof_Scaladz <cskladzien@gmail.com>
2018-07-26 00:35:46 +0000

    udpate

---
+++ projects/matchy/functions.js
@@ -0,0 +1,81 @@
+/**
+ * Part 2
+ *
+ * In this file, we're going to create some
+ * Functions to work with our data created in
+ * data.js.
+ *
+ * See the README for detailed instructions,
+ * and read every instruction carefully.
+ */
+
+//////////////////////////////////////////////////////////////////////
+// Step 1 - Search ///////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+function search (animals, name){
+    //console.log(name.toLowerCase());
+    for(var i = 0; i < animals.length; i++){
+        //console.log(animals[i].name);
+        if(animals[i].name === name || animals[i].name.toLowerCase() === name){
+            return animals[i];
+        }
+    }    
+    return null;
+}
+
+
+//////////////////////////////////////////////////////////////////////
+// Step 2 - Replace //////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+function replace(animals, name, replacement){
+    for(var i = 0; i < animals.length; i++){
+        if(animals[i].name === name){
+            animals[i] = replacement;
+        }
+    }
+}
+
+
+//////////////////////////////////////////////////////////////////////
+// Step 3 - Remove ///////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+function remove(animals, name){
+    for(var i = 0; i < animals.length; i++){
+        if(animals[i].name === name){
+            animals.splice(i, 1);
+        }    
+    }
+}
+
+
+//////////////////////////////////////////////////////////////////////
+// Step 4 - Create ///////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+function add(animals, animal){
+    for(var i=0; i<animals.length; i++){
+        if (animals[i].name === animal.name) return;
+
+        else if(animal.name != 0 && animal.species != 0){
+            animals.push(animal);
+        }
+    }
+}
+
+
+/**
+ * You did it! You're all done with Matchy!
+ */
+
+
+
+//////////////////////////////////////////////////////////////////////
+// DON'T REMOVE THIS CODE ////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////
+var search, replace, remove, add;
+if((typeof process !== 'undefined') &&
+   (typeof process.versions.node !== 'undefined')) {
+    module.exports.search = search || null;
+    module.exports.replace = replace || null;
+    module.exports.remove = remove || null;
+    module.exports.add = add || null;
+}
